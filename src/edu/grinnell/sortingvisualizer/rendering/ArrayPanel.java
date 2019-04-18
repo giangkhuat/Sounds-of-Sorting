d8d358d3b4f899cdd8f35360d190f89c0374fce3
Khuat <khuatgia@holberton.cs.grinnell.edu>
2019-04-17 19:33:49 -0500

    third commit wednesday

--- src/edu/grinnell/sortingvisualizer/rendering/ArrayPanel.java
+++ src/edu/grinnell/sortingvisualizer/rendering/ArrayPanel.java
@@ -3,53 +3,62 @@ package edu.grinnell.sortingvisualizer.rendering;
 import java.awt.Color;
 import java.awt.Dimension;
 import java.awt.Graphics;
-
 import javax.swing.JPanel;
 import edu.grinnell.sortingvisualizer.audio.NoteIndices;
 
 @SuppressWarnings("serial")
 public class ArrayPanel extends JPanel {
 
-    private NoteIndices notes;
-    
-    /**
-     * Constructs a new ArrayPanel that renders the given note indices to
-     * the screen.
-     * @param notes the indices to render
-     * @param width the width of the panel
-     * @param height the height of the panel
-     */
-    public ArrayPanel(NoteIndices notes, int width, int height) {
-        this.notes = notes;
-        this.setPreferredSize(new Dimension(width, height));
-    }
+  private NoteIndices notes;
 
-    @Override
-    public void paintComponent(Graphics g) {
-      //set background to white
+  /**
+   * Constructs a new ArrayPanel that renders the given note indices to the screen.
+   * 
+   * @param notes the indices to render
+   * @param width the width of the panel
+   * @param height the height of the panel
+   */
+  public ArrayPanel(NoteIndices notes, int width, int height) {
+    this.notes = notes;
+    this.setPreferredSize(new Dimension(width, height));
+  }
+
+  @Override
+  public void paintComponent(Graphics g) {
+    // draw the notes on the panel
+    Integer[] noteIndices = notes.getNotes();
+    int noteWidth = this.getWidth() / notes.notesLength();
+    // divide height into n parts
+    int heightPart = this.getHeight() / notes.notesLength();
+    // set color
+    int colorScale = 10;
+
+    for (int i = 0; i < notes.notesLength(); i++) {
+      // scale height of rectangle using heightPart
+      int noteHeight = heightPart * (noteIndices[i] + 1);
+      // if the note is highlighted
+      if (notes.isHighlighted(i)) {
+        g.setColor(Color.CYAN);
+      } else {
+        // scale color according to value
+        int comp = noteIndices[i] * colorScale;
+        g.setColor(new Color(0, comp, comp));
+      }
+      // build the rectangle
+      g.fillRect(noteWidth * i, this.getHeight() - noteHeight, noteWidth, noteHeight);
+      //build the complement of the rectangle
       g.setColor(Color.WHITE);
-      
-      // draw the notes on the panel
-        Integer[] noteIndices = notes.getNotes();
-        int noteWidth = this.getWidth()/ notes.notesLength();
-        //divide height into n parts
-        int heightPart = this.getHeight()/notes.notesLength();
-        // set color
-        int colorScale = 10;
-        
-        for (int i = 0 ; i < notes.notesLength(); i++) {
-          // scale height of rectangle using heightPart
-          int noteHeight = heightPart * (noteIndices[i] + 1);
-          // if the note is highlighted
-          if (notes.isHighlighted(i)) {
-            g.setColor(Color.CYAN);
-          } else { 
-            //scale color according to value
-            int comp = noteIndices[i]*colorScale;
-            g.setColor(new Color(0, comp, comp));
-          }
-          //build the rectangle
-          g.fillRect(noteWidth * i, this.getHeight() - noteHeight, noteWidth, noteHeight);
-        }
+      g.fillRect(noteWidth * i, 0, noteWidth, this.getHeight() - noteHeight);
     }
+    /*
+    int lastNote = notes.notesLength() - 1;
+ // build the rectangle
+    int comp = noteIndices[lastNote] * colorScale;
+    g.setColor(new Color(0, comp, comp));
+    g.fillRect(noteWidth * lastNote, this.getHeight() - (heightPart * (noteIndices[lastNote]) + 1), noteWidth, (noteIndices[lastNote] + 1));
+    lastNote--;
+    comp = noteIndices[lastNote] * colorScale;
+    g.fillRect(noteWidth * lastNote, this.getHeight() - (heightPart * (noteIndices[lastNote]) + 1), noteWidth, (noteIndices[lastNote] + 1));
+    */
+  }
 }
